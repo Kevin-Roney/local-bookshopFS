d0ca258cde54974417c3a90ccd89be78a4257974
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-13 16:31:29 -0700

    added test for /books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,14 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /books should return a list of books rendering id, title and release year', () => {
+    const res = request(app).get('/books');
+    const books = Book.getAll();
+    expect(res.body).toEqual(books);
   });
   afterAll(() => {
     pool.end();
