f32039052c4e070db67252d8a0231c81490dbc01
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-14 12:40:07 -0700

    fixed tests for getAll functions

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,21 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Book = require('../lib/models/Book');
+
 
 describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('GET /books should return a list of books rendering id, title and release year', () => {
-    const res = request(app).get('/books');
-    const books = Book.getAll();
-    expect(res.body).toEqual(books);
+  it('GET /books should return a list of books rendering id, title and release year', async () => {
+    const resp = await request(app).get('/books');
+    expect(resp.status).toBe(200);
+    expect(resp.body[0].id).toBe('1');
+    // expect(resp.body.length).toEqual(8);
   });
-  it('Get /authors should return a list of authors rendering id, name and books', () => {
-    const res = request(app).get('/authors');
-    const authors = Book.getAll();
-    expect(res.body).toEqual(authors);
+  it('Get /authors should return a list of authors rendering id, name and books', async () => {
+    const resp = await request(app).get('/authors');
+    expect(resp.status).toBe(200);
+    expect(resp.body[0].id).toBe('1');
   });
   afterAll(() => {
     pool.end();
