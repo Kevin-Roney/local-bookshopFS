c655c5b10fcb6794df5ea0fb23492eb1b1808c45
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-13 17:49:40 -0700

    added controllers for books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Book = require('../lib/models/Book');
 
 describe('books routes', () => {
   beforeEach(() => {
